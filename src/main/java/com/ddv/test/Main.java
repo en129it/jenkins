27cac3a6ad81b0f4ca131f61f9f77647d8719267
ddv3 <ddv@ddv.com>
2019-03-17 23:01:43 +0100

    First commit

---
+++ src/main/java/com/ddv/test/Main.java
@@ -0,0 +1,13 @@
+package com.ddv.test;
+
+public class Main {
+
+	private void start() {
+		System.out.println("Hello Toto");
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Main t = new Main();
+		t.start();
+	}
+}
